93e1cec1c62b0413875e2ba378100a048e58c29f
Jonah <107316154@gms.tcu.edu.tw>
2019-12-21 15:02:12 +0800

    Migration
    Previously version was pointing towards r3 server. Made this move for wider Restful API coverage. And for better differentiation between diffirent FHIR resources

---
+++ FHIR_practitionerRole.js
@@ -0,0 +1,26 @@
+﻿function practitionerRole() {
+    var text = {
+        "resourceType": "PractitionerRole",
+        "id": "",
+        "practitioner": {
+            "reference": "Practitioner/1948445"
+        },
+        "organization": {
+            "reference": "Organization/jonah"
+        },
+        "code": [{
+            "coding": [{
+                "system": "http://hl7.org/fhir/practitioner-role",
+                "code": "nurse"
+            }]
+        }]
+    }
+    practitionerId = document.getElementById("pid").value;
+    text.practitioner.reference = "Practitioner/" + practitionerId;
+    text.code[0].coding[0].code = document.getElementById("role").value;
+    text.id = practitionerId + "HFC";
+    var myJSON = JSON.stringify(text);
+    //HTTPPostData("http://hapi.fhir.org/baseDstu3/PractitionerRole", myJSON, "practitionerRole");
+    var url = "http://hapi.fhir.org/baseDstu3/PractitionerRole/" + text.id;
+    HTTPPutData(url, myJSON, "practitionerRole");
+}
